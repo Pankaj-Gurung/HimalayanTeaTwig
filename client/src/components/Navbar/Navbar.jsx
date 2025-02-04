a09d516d2ba241e2d3f083a7eff12e921b45bd95
Pankaj Gurung <grgpankaj004@gmail.com>
2025-02-04 16:17:48 +0545

    New: Add Truck Icon on Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import Home from "../../pages/Home/Home";
 import Shop from "../../pages/Shop/Shop";
 import About from "../../pages/About/About";
 import "./Navbar.scss";
+import Nepalflag from "../../Images/nepal-flag-icon.svg?url";
+import truck from "../../Images/truck.gif";
 
 const Navbar = (props) => {
   const {} = props;
@@ -12,12 +14,21 @@ const Navbar = (props) => {
     <>
       <div className="nav-container">
         <div className="nav-wrapper">
+          <div className="top-nav">
+            <span>Delivery all over Nepal!</span>{" "}
+            <p>
+              <img src={truck} alt="" className="truck" />
+            </p>
+          </div>
           <nav>
             <Link to={"/home"}>
               <a>Home</a>{" "}
             </Link>
             <Link to={"/shop"}>
               <a>Shop</a>{" "}
+            </Link>
+            <Link>
+              <a>Contact Us</a>
             </Link>
             <Link to={"/aboutus"}>
               <a>About Us</a>
