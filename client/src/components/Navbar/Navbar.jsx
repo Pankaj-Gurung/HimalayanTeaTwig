426489d9df154d770615a016aff89ce7b0142677
Pankaj Gurung <grgpankaj004@gmail.com>
2025-02-07 14:38:28 +0545

    Update: change in navba

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import {Link} from "react-router-dom";
 import "./Navbar.scss";
 import Nepalflag from "../../Images/nepal-flag-icon.svg?url";
 import truck from "../../Images/truck.gif";
+import Button from '../Button/Button'
+import logo from '../../Images/logo.png'
 
 const Navbar = (props) => {
   const {} = props;
@@ -10,28 +12,31 @@ const Navbar = (props) => {
   return (
     <>
       <div className="nav-container">
-        <div className="nav-wrapper">
+        
           <div className="top-nav">
             <span>Delivery all over Nepal!</span>{" "}
             <p>
               <img src={truck} alt="" className="truck" />
             </p>
           </div>
-          <nav className="nav-link-wrap">
-            <Link to={"/home"}>
-              Home{" "}
-            </Link>
-            <Link to={"/shop"}>
-              Shop{" "}
-            </Link>
-            <Link to={"/contactus"}>
-              Contact Us
-            </Link>
-            <Link to={"/aboutus"}>
-              About Us
-            </Link>
-          </nav>
-        </div>
+
+          <nav>
+  <div className="logo"><img src={logo} alt="Logo" /></div>
+
+  <div className="link-wrapper">
+    <Link to={"/home"}>Home</Link>
+    <Link to={"/shop"}>Shop</Link>
+    <Link to={"/contactus"}>Contact Us</Link>
+    <Link to={"/aboutus"}>About Us</Link>
+  </div>
+
+  <div className="button-wrapper">
+    <Button>Login</Button>
+    <Button>Signup</Button>
+  </div>
+</nav>
+
+
       </div>
     </>
   );
