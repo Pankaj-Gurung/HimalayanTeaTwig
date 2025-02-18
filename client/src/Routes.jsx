c674da90a3f3aa7c5c6728d2eecd8334d371daae
Pankaj Gurung <grgpankaj004@gmail.com>
2025-02-18 21:27:21 +0545

    update: change naming of the components and page according to convention

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Home from "./pages/Home/Home";
-import Shop from "./pages/Shop/Shop";
-import About from "./pages/About/About";
-import Contactus from './pages/Contact/Contactus';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home.page";
+import Shop from "./pages/shop/shop.page";
+import About from "./pages/about.page";
+import Contactus from "./pages/contact/contactus.page";
 
 const AppRoutes = () => {
   return (
     <>
-    <Routes>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/shop" element={<Shop />}></Route>
-          <Route path="/aboutus" element={<About />}></Route>
-          <Route path="/contactus" element={<Contactus />}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+        <Route path="/aboutus" element={<About />}></Route>
+        <Route path="/contactus" element={<Contactus />}></Route>
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
