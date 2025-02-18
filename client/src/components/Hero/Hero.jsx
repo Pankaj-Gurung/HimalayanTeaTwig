6f691f64eff75e399cef6483ce1b99ccd569ea45
Pankaj Gurung <grgpankaj004@gmail.com>
2025-02-18 13:14:39 +0545

    update: add hero section

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import heroimg1 from '../../Images/heroimg1'
+const Hero = () => {
+  return (
+    <main className='hero-container'>
+    <img src={heroimg1} alt="" />
+    <img src={''} alt="" />
+    </main>
+  )
+}
+
+export default Hero
