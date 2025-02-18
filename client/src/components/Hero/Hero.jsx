9bdb4cd7a51b342a4cafd3ac19f5621e48fef7ec
Pankaj Gurung <grgpankaj004@gmail.com>
2025-02-18 14:18:28 +0545

    update: changes made on the hero section

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import heroimg1 from '../../Images/heroimg1'
+import heroimg1 from '../../Images/hero1.png'
+import heroimg101 from '../../Images/hero1.1.jpg' 
+import './Hero.scss'
+
 const Hero = () => {
   return (
     <main className='hero-container'>
-    <img src={heroimg1} alt="" />
-    <img src={''} alt="" />
+    <div className='firstimg-wrapper'>
+    <img src={heroimg101} alt="" className='first-img101' />
+     <div className='img1'>
+        <h1>Original Tea</h1>
+        <h1>From Ilam</h1>
+        <p>Reasonable Pricing</p>
+        <p>Hand Picked</p>   
+    </div>   
+    </div>
+
+    <div className='secondimg-wrapper'></div>
+    <div className='thirdimg-wrapper'></div>
     </main>
   )
 }
