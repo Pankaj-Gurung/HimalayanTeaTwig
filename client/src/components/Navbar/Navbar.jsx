6e7cc633863e75c7bb5cb3fa7412fc4264adab7a
Pankaj Gurung <grgpankaj004@gmail.com>
2025-02-18 13:03:55 +0545

    update: change on logo size, and add icons for account and cart

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,8 +3,10 @@ import {Link} from "react-router-dom";
 import "./Navbar.scss";
 import Nepalflag from "../../Images/nepal-flag-icon.svg?url";
 import truck from "../../Images/truck.gif";
-import Button from '../Button/Button'
 import logo from '../../Images/logo.png'
+import { CgProfile } from "react-icons/cg";
+import { LuShoppingCart } from "react-icons/lu";
+
 
 const Navbar = (props) => {
   const {} = props;
@@ -19,20 +21,17 @@ const Navbar = (props) => {
               <img src={truck} alt="" className="truck" />
             </p>
           </div>
-          <nav>
-
+  <nav className="bottom-nav">
   <Link to={'/home'}><div className="logo"><img src={logo} alt="Logo" /></div></Link>
-
   <div className="link-wrapper">
     <Link className="nav-links" to={"/home"} >HOME</Link>
     <Link className="nav-links" to={"/shop"}>SHOP</Link>
     <Link className="nav-links" to={"/contactus"}>CONTACT US</Link>
     <Link className="nav-links" to={"/aboutus"}>ABOUT US</Link>
   </div>
-
   <div className="right-link">
-  <Link>ACCOUNT</Link>
-  <Link>CART</Link>
+  <Link><CgProfile size={20}/><span>ACCOUNT</span></Link>
+  <Link><LuShoppingCart size={20}/><span>CART</span></Link>
   </div>
 </nav>
     </main>
