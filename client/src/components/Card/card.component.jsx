93c6211eb18a63bc635901adb9cea684b9633821
Pankaj Gurung <grgpankaj004@gmail.com>
2025-02-18 22:02:34 +0545

    update: add props in the card component

--- client/src/components/Card/card.component.jsx
+++ client/src/components/Card/card.component.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./card.scss";
 
-const Card = () => {
+const Card = (props) => {
+  const { image, title, content, price } = props;
   return (
     <main className="card-container">
       <div className="card-wrapper">
-
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{content}</p>
+        <span>{price}</span>
       </div>
     </main>
   );
