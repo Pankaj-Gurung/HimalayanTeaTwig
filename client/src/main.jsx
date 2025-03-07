ed1762dcf40e27bc6c60201444ec9df82e072930
Pankaj Gurung <grgpankaj004@gmail.com>
2025-03-07 20:10:11 +0545

    update: add global scss

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
-      <Routes />
+      <AppRoutes />
     </BrowserRouter>
   </StrictMode>
 );
