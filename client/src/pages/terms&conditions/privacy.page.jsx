eef6d35d8de9df719a109444c4def7959158164f
Pankaj Gurung <grgpankaj004@gmail.com>
2025-03-17 21:37:49 +0545

    update: make changes in privacy page

--- client/src/pages/terms&conditions/privacy.page.jsx
+++ client/src/pages/terms&conditions/privacy.page.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Privacy = () => {
-  return (
-    <div>Privacy</div>
-  )
-}
+  return <></>;
+};
 
-export default Privacy
+export default Privacy;
